51b69a8777cb8fe5039975c0cd111f7d2f5d3cd1
suoerstar-zimtk <zimtk4@gmail.com>
2025-12-24 17:09:22 +0200

    Mr Frank is the best

---
+++ pair.js
@@ -0,0 +1,101 @@
+const PastebinAPI = require('pastebin-js');
+const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const { makeid } = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router();
+const pino = require('pino');
+const {
+    default: Mbuvi_Tech,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require('@whiskeysockets/baileys');
+
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+
+    async function Mbuvi_MD_PAIR_CODE() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            let Pair_Code_By_Mbuvi_Tech = Mbuvi_Tech({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
+                },
+                // FIXED VERSION ONLY
+                version: [2, 3001, 7],
+                printQRInTerminal: false,
+                logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
+                browser: Browsers.macOS('Chrome')
+            });
+
+            if (!Pair_Code_By_Mbuvi_Tech.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await Pair_Code_By_Mbuvi_Tech.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            Pair_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds);
+            Pair_Code_By_Mbuvi_Tech.ev.on('connection.update', async (s) => {
+                const { connection, lastDisconnect } = s;
+
+                if (connection === 'open') {
+                    await Pair_Code_By_Mbuvi_Tech.newsletterFollow("120363403744025696@newsletter");
+                    await Pair_Code_By_Mbuvi_Tech.groupAcceptInvite("KjEgiX13hLoDD7sE7STmGR");
+                    await delay(5000);
+
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    await delay(800);
+                    let b64data = Buffer.from(data).toString('base64');
+
+                    let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(
+                        Pair_Code_By_Mbuvi_Tech.user.id,
+                        { text: 'XHYPHER:~' + b64data }
+                    );
+
+                    let Mbuvi_MD_TEXT = `
+        
+
+        
+🟢 Session verified successfully!
+
+*TYPE:* BASE64
+*STATUS:* Active and Working ✅
+
+Don't Forget To Give Star⭐ To My Repo
+______________________________`;
+
+                                        await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: Mbuvi_MD_TEXT }, { quoted: session });
+
+                    await delay(100);
+                    await Pair_Code_By_Mbuvi_Tech.ws.close();
+                    return await removeFile('./temp/' + id);
+                } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    Mbuvi_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log('Service restarted');
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: 'Service Currently Unavailable' });
+            }
+        }
+    }
+
+    return await Mbuvi_MD_PAIR_CODE();
+});
+
+module.exports = router;
